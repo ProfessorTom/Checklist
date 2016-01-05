f6973efaca03f3650273f0512238931fb2972dbc
Tomas Gallucci <professortom@gmail.com>
2016-01-04 18:54:30 -0600

    makes the keyboard be on screen when the Add Item view becomes visible by making the text field become the first responder in viewWillAppear. Also, made the "Done" return button disabled unless there is at least one character in the text field. Next step: make the AddItemViewController delegate for the text field.

--- Checklists/AddItemViewControler.swift
+++ Checklists/AddItemViewControler.swift
@@ -11,6 +11,11 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -19,7 +24,7 @@ class AddItemViewController: UITableViewController {
         print("luser gave us: \(textField.text!)")
         dismissViewControllerAnimated(true, completion: nil)
     }
-     
+    
     @IBOutlet weak var textField: UITextField!
     
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
